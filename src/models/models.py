f71d6ae8d00f4663bcc92d29afee665274ad03c0
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2026-01-09 09:05:35 -0300

    iniciando

---
+++ src/models/models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Usuario(BaseModel):
+    id: Optional[str] = None 
+    nome: str
+    telefone: str
+    meus_produtos: List[Produtos]
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+
+class Produtos(BaseModel):
+    id: Optional[str] = None
+    Usuario: Usuario
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produtos
+    quantidade: int 
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str] = 'Sem observações'

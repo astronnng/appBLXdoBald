5e2443edc8ded2dc0cff0effa25173cc2075ba19
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2026-01-21 09:47:59 -0300

    Inserido classe Config com ORM Mode

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -19,6 +19,10 @@ class Produtos(BaseModel):
     preco: float
     disponivel: bool = False
 
+    class Config():
+        orm_mode = True
+
+
 class Pedido(BaseModel):
     id: Optional[str] = None
     usuario: Usuario
